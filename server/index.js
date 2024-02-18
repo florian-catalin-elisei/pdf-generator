51ac16da349a80a0e3cf8bf83042674b42f6e40e
elisei-florian-catalin <catalinelisei2002@gmail.com>
2024-02-18 10:27:55 +0200

    Add root route

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("PDF Generator Server");
+});
+
 app.post("/create-pdf", async (req, res) => {
   try {
     const browser = await puppeteer.launch({ headless: "new" });
